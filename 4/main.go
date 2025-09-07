29f7c187282a5b7d18679fa5f894f3c9f52d2d07
bdrlv <aleksej.tp@gmail.com>
2025-09-07 19:54:02 +0500

    add l1.4 (l1.3 with ctx)

---
+++ 4/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"strconv"
+	"time"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		log.Println("ошибка некорректного количества аргументов. Укажите количество воркеров в качестве аргумента.")
+		os.Exit(1)
+	}
+
+	wn, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		log.Printf("некорректный формат аргумента, %v. Значение должно быть положительным целочисенным и больше, либо равное 1.", err)
+		os.Exit(1)
+	}
+
+	if wn <= 0 {
+		log.Println("значение колечства воркеров должно быть >= 1")
+		os.Exit(1)
+	}
+
+	log.Printf("L1.4 - Запущено воркеров: %d\n", wn)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	ch := make(chan int)
+
+	for i := 0; i < wn; i++ {
+		go func(ctx context.Context, worker_id int, data <-chan int) {
+			for {
+				select {
+				case val, ok := <-data:
+					if !ok {
+						log.Println("Канал закрыт")
+						return
+					}
+					log.Printf("Воркер: %d, Полученное значение: %d\n", worker_id, val)
+				case <-ctx.Done():
+					log.Println("Команда завершения работы")
+					return
+				}
+
+			}
+		}(ctx, i, ch)
+	}
+
+	for {
+		ch <- time.Now().Nanosecond()
+		time.Sleep(100 * time.Millisecond)
+	}
+}
