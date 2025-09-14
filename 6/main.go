4d619243e87d44cbf1d9c49c8e7e30e4302b9001
bdrlv <aleksej.tp@gmail.com>
2025-09-14 21:16:13 +0500

    l1.6: work 2 variants

--- 6/main.go
+++ 6/main.go
@@ -8,22 +8,47 @@ import (
 
 func main() {
 
+	// Остановка по уведомлению через канал (вообще сделал гибрит с условием)
+	stopByChannel()
+	// Остановка по условию
+	stopByCondition()
+}
+func stopByChannel() {
+	// Пример: Пока не будет получено 20 значений из канала
+	n := 20 // лимит получаемых значений
 	ctxBG := context.Background()
 	dataChan := make(chan int)
-
-	// Остановка по усовию
-	// Пример: Пока не будет получено 20 значений из канала
-	limitN := 20
-	ctx1, cancel1 := context.WithCancel(ctxBG)
+	ctx, cancel1 := context.WithCancel(ctxBG)
 	defer cancel1()
-	sig1 := make(chan int)
-	go stopByCondition(ctx1, dataChan, limitN, sig1)
+	sig := make(chan int, 1)
 
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				log.Println("Остановка горутины stopByChannel")
+				return
+			case val, ok := <-dataChan:
+				if !ok {
+					log.Println("stopByChannel: канал закрыт")
+					return
+				}
+				n--
+				log.Printf("stopByChannel получила %d, осталось получить %d значений.\n", val, n)
+				if n == 0 {
+					sig <- 1
+				}
+
+			}
+		}
+	}()
+
+	// Передача данных
 	for {
 		select {
-		case <-ctx1.Done():
+		case <-ctx.Done():
 			return
-		case <-sig1:
+		case <-sig:
 			cancel1()
 		default:
 			dataChan <- time.Now().Nanosecond()
@@ -32,23 +57,33 @@ func main() {
 	}
 }
 
-func stopByCondition(ctx context.Context, ch <-chan int, n int, sig chan<- int) {
-	for {
-		select {
-		case <-ctx.Done():
-			log.Println("Остановка горутины stopByCondition")
-			return
-		case val, ok := <-ch:
+func stopByCondition() {
+
+	dataChan := make(chan int)
+	n := 20
+	chanClosed := make(chan struct{}) // Будем использовать для обратной связи из горутины, чтобы выйти из функции
+
+	go func() {
+		for n > 0 { // Основное условие, при срабатывании которого горутина будет выходить из цикла и посылать обратную связь
+			val, ok := <-dataChan
 			if !ok {
-				log.Println("stopByCondition: канал закрты")
+				log.Println("stopByCondition: канал закрыт")
 				return
 			}
 			n--
-			log.Println(val, n)
-			if n == 0 {
-				sig <- 1
-			}
+			log.Printf("stopByCondition получила %d, осталось получить %d значений.\n", val, n)
 
+		}
+		log.Println("Остановка горутины stopByCondition")
+		close(chanClosed)
+	}()
+
+	for {
+		select {
+		case <-chanClosed: // выходим из функции при получении сигнала обратной связи
+			return
+		case dataChan <- time.Now().Nanosecond(): // Вынес в кейс, т.к. оставляя в default получаем дедлок
+			time.Sleep(100 * time.Nanosecond)
 		}
 	}
 }
