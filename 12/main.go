171a11783fba699377b8a6ded6ad341cc4b69950
bdrlv <aleksej.tp@gmail.com>
2025-10-21 14:06:51 +0500

    L1.12 logic

--- 12/main.go
+++ 12/main.go
@@ -1,5 +1,23 @@
 package main
 
+import "fmt"
+
 func main() {
+	input := []string{"cat", "cat", "dog", "cat", "tree"}
+	var result []string
+
+	set := make(map[string]bool)
+	for _, item := range input {
+		set[item] = true
+	}
+
+	for key, val := range set {
+		if val {
+			result = append(result, key)
+		}
+	}
+
+	// fmt.Println(input)
+	fmt.Println(result)
 
 }
