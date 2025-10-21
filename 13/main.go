1c04019a545492f24bfa81ca458d72ca6b0dc267
bdrlv <aleksej.tp@gmail.com>
2025-10-21 15:01:50 +0500

    L1.13

--- 13/main.go
+++ 13/main.go
@@ -1,1 +1,28 @@
 package main
+
+import "fmt"
+
+func main() {
+	a := 2
+	b := 3
+
+	ver1(a, b)
+	ver2(a, b)
+
+}
+
+func ver1(a, b int) {
+	a = a + b
+	b = a - b
+	a = a - b
+
+	fmt.Printf("Результат обмена через сложение/вычитание: a=%d, b=%d\n", a, b)
+}
+
+func ver2(a, b int) {
+	a = a ^ b
+	b = a ^ b
+	a = a ^ b
+
+	fmt.Printf("Результат обмена через XOR: a=%d, b=%d\n", a, b)
+}
